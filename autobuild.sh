4170f4be39387f925d48cd2c2aaba5840561c03f
Lee Azzarello <lee@rockingtiger.com>
2024-04-28 00:21:25 -0700

    add first draft scripts

---
+++ autobuild.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+echo "this script changes system settings as root for bootup and package settings"
+
+apt-get update && apt-get upgrade -y
+apt-get install -y landscape-client
+
+# REF: https://askubuntu.com/questions/1322292/how-do-i-turn-off-automatic-updates-completely-and-for-real
+cat <<-EOF > /etc/apt/apt.conf.d/20auto-upgrades
+APT::Periodic::Update-Package-Lists "0";
+APT::Periodic::Download-Upgradeable-Packages "0";
+APT::Periodic::AutocleanInterval "0";
+APT::Periodic::Unattended-Upgrade "0";
+EOF
+
+apt remove -y unattended-upgrades
+echo "enabled=0" > /etc/default/apport
+
+cat <<-EOF > /etc/default/grub
+GRUB_DEFAULT=0
+GRUB_TIMEOUT_STYLE=hidden
+GRUB_TIMEOUT=0
+GRUB_DISTRIBUTOR=`lsb_release -i -s 2> /dev/null || echo Debian`
+GRUB_CMDLINE_LINUX_DEFAULT="quiet splash"
+GRUB_CMDLINE_LINUX=""
+GRUB_RECORDFAIL_TIMEOUT=0
+GRUB_DISABLE_RECOVERY="true"
+EOF
+
+update-grub
---
+++ user-settings.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+set -x
+echo "this script changes user mode settings for game playback and visual appearance"
+
+gsettings set org.gnome.desktop.bluetooth bluetooth-enabled false
+gsettings set org.gnome.nautilus.desktop trash-icon-visible false
+gsettings set org.gnome.settings-daemon.plugins.power idle-dim false
+gsettings set org.gnome.desktop.session idle-delay 0
+gsettings set org.gnome.desktop.background primary-color '#000000'
+gsettings set org.gnome.desktop.background picture-uri ''
+gsettings set org.gnome.desktop.notifications show-banners false
